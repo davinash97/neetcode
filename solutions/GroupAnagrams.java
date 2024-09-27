20b6027a9a4b69547dd9e9e17fbb52b1c653bec5
davinash97 <avinashd2904@gmail.com>
2024-09-27 20:44:03 +0530

    49. Group Anagrams

---
+++ solutions/GroupAnagrams.java
@@ -0,0 +1,19 @@
+class Solution {
+	public List<List<String>> groupAnagrams(String[] strs) {
+		Map<String, List<String>> map = new HashMap<>();
+
+		for(String s: strs) {
+			char[] ch = s.toCharArray();
+			Arrays.sort(ch);
+
+			String sortedStr = new String(ch);
+
+			if(!map.containsKey(sortedStr)) {
+				map.put(sortedStr, new ArrayList<>());
+			}
+			map.get(sortedStr).add(s);
+		}
+
+		return new ArrayList<>(map.values());
+	}
+}
